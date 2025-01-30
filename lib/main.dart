75084c710186524aee31b9378dd4f351430bfc71
shashwat-naik <naikshashwat3@gmail.com>
2025-01-30 21:00:01 +0530

    Minor change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CRUD Masterclass',
+      title: 'CRUD Practice',
       theme: ThemeData(
         // This is the theme of your application.
         //
